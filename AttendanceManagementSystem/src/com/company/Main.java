7d9ae04d16f58dc4ac8b7a260d9aebf99894d84c
mertsariozkan <mert.sariozkan@hotmail.com>
2019-04-25 19:04:00 +0300

    Main class correction

--- AttendanceManagementSystem/src/com/company/Main.java
+++ AttendanceManagementSystem/src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        DatabaseOperations db = new DatabaseOperations();
-        System.out.println(db.getStudentDepartment(1));
-        //new DatabaseOperations(); //Accessing the database & its operations.
-        //new InitialScreen(); 	  //Start app with initial screen, because first thing we want to do is to select which type of user we are.
+        new DatabaseOperations(); //Accessing the database & its operations.
+        new InitialScreen(); 	  //Start app with initial screen, because first thing we want to do is to select which type of user we are.
     }
 }
 
